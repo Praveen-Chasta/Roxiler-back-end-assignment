33eded1726e54b7d24a99b7cfa1d5bfe454d225e
Praveen Chasta <praveen.chastaa@gmail.com>
2023-12-03 14:40:33 +0000

    Roxiler

---
+++ README.md
@@ -0,0 +1,20 @@
+1). Create an API to initialize the database:
+
+Fetch the JSON data from the third-party API.
+Use the fetched data to seed the database with product transaction records.
+
+2). Create an API for statistics:
+
+Calculate and return the total sale amount of products sold in the selected month.
+Calculate and return the total number of products sold and not sold in the selected month.
+
+3). Create an API for a bar chart:
+
+Generate a bar chart showing the number of products falling in different price ranges for the selected month.
+The price ranges are predefined, such as 0-100, 101-200, etc.
+
+4). Create an API for a pie chart:
+
+Generate a pie chart showing the unique categories of products and the number of items from each category for the selected month.
+
+5). Create an API that fetches data from the three APIs mentioned above and combines the responses to provide a comprehensive report on product transactions for the selected month.
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const initializeDbAndServer = async () => {
     });
 
     await database.run(`
-      CREATE TABLE IF NOT EXISTS [transaction] (
+      CREATE TABLE IF NOT EXISTS transactionTable (
         id INTEGER PRIMARY KEY,
         title VARCHAR(255),
         price DECIMAL(10, 2),
@@ -58,7 +58,7 @@ async function getData(url) {
         dateOfSale: d.dateOfSale,
       };
 
-      let sql = `INSERT INTO transaction(id,title,price,description,category,image,sold,dateOfSale) VALUES(?,?,?,?,?,?,?,?)`;
+      let sql = `INSERT OR IGNORE INTO transactionTable (id,title,price,description,category,image,sold,dateOfSale) VALUES(?,?,?,?,?,?,?,?)`;
       try {
         database.run(sql, [
           userData.id,
@@ -79,4 +79,184 @@ async function getData(url) {
   }
 }
 
-getData("https://s3.amazonaws.com/roxiler.com/product_transaction.json");
+setTimeout(() => {
+  getData("https://s3.amazonaws.com/roxiler.com/product_transaction.json");
+}, 5000);
+
+app.get("/transactions", async (req, res) => {
+  try {
+    const { search = "", page = 1, perPage = 10 } = req.query;
+
+    const offset = (page - 1) * perPage;
+
+    let searchCondition = "";
+    if (search) {
+      searchCondition = `
+        WHERE title LIKE '%${search}%' OR
+              description LIKE '%${search}%' OR
+              price LIKE '%${search}%'
+      `;
+    }
+
+    const query = `
+      SELECT * FROM transactionTable
+      ${searchCondition}
+      ORDER BY id
+      LIMIT ${perPage} OFFSET ${offset}
+    `;
+
+    const transactions = await database.all(query);
+
+    res.json(transactions);
+  } catch (error) {
+    console.error(`List Transactions Error: ${error.message}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/statistics", async (req, res) => {
+  try {
+    const { month } = req.query;
+
+    const totalSaleAmountResult = await database.get(
+      "SELECT SUM(price) AS totalSaleAmount FROM transactionTable WHERE strftime('%Y-%m', dateOfSale) = ? AND sold = 1",
+      [month]
+    );
+    const totalSaleAmount = totalSaleAmountResult.totalSaleAmount || 0;
+
+    const totalSoldResult = await database.get(
+      "SELECT COUNT(*) AS totalSold FROM transactionTable WHERE strftime('%Y-%m', dateOfSale) = ? AND sold = 1",
+      [month]
+    );
+    const totalSold = totalSoldResult.totalSold || 0;
+
+    const totalNotSoldResult = await database.get(
+      "SELECT COUNT(*) AS totalNotSold FROM transactionTable WHERE strftime('%Y-%m', dateOfSale) = ? AND sold = 0",
+      [month]
+    );
+    const totalNotSold = totalNotSoldResult.totalNotSold || 0;
+
+    res.json({
+      totalSaleAmount,
+      totalSold,
+      totalNotSold,
+    });
+  } catch (error) {
+    console.error(`Statistics Error: ${error.message}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/bar-chart", async (req, res) => {
+  try {
+    const { month } = req.query;
+
+    const priceRanges = [
+      { min: 0, max: 100 },
+      { min: 101, max: 200 },
+      { min: 201, max: 300 },
+      { min: 301, max: 400 },
+      { min: 401, max: 500 },
+      { min: 501, max: 600 },
+      { min: 601, max: 700 },
+      { min: 701, max: 800 },
+      { min: 801, max: 900 },
+      { min: 901, max: Infinity },
+    ];
+
+    const priceRangesData = await Promise.all(
+      priceRanges.map(async (range) => {
+        const { min, max } = range;
+
+        const result = await database.get(
+          "SELECT COUNT(*) AS count FROM transactionTable WHERE strftime('%Y-%m', dateOfSale) = ? AND price >= ? AND price <= ?",
+          [month, min, max]
+        );
+
+        return { range: `${min}-${max}`, count: result.count || 0 };
+      })
+    );
+
+    res.json(priceRangesData);
+  } catch (error) {
+    console.error(`Bar Chart Error: ${error.message}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/pie-chart", async (req, res) => {
+  try {
+    const { month } = req.query;
+
+    const categoriesData = await database.all(
+      "SELECT category, COUNT(*) AS count FROM transactionTable WHERE strftime('%Y-%m', dateOfSale) = ? GROUP BY category",
+      [month]
+    );
+
+    res.json(
+      categoriesData.map(({ category, count }) => ({ [category]: count }))
+    );
+  } catch (error) {
+    console.error(`Pie Chart Error: ${error.message}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const getTotalStatistics = async (month) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/statistics?month=${month}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching total statistics: ${error.message}`);
+    throw error;
+  }
+};
+
+const getBarChartData = async (month) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/bar-chart?month=${month}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching bar chart data: ${error.message}`);
+    throw error;
+  }
+};
+
+const getPieChartData = async (month) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/pie-chart?month=${month}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching pie chart data: ${error.message}`);
+    throw error;
+  }
+};
+
+app.get("/combined-data", async (req, res) => {
+  try {
+    const { month } = req.query;
+
+    const [totalStatistics, barChartData, pieChartData] = await Promise.all([
+      getTotalStatistics(month),
+      getBarChartData(month),
+      getPieChartData(month),
+    ]);
+
+    const combinedResponse = {
+      totalStatistics,
+      barChartData,
+      pieChartData,
+    };
+
+    res.json(combinedResponse);
+  } catch (error) {
+    console.error(`Combined Data Error: ${error.message}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
